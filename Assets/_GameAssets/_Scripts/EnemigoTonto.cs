2f2ac6c7a4fecdc50d4b3db1df1850886ce9e14c
Alicia Conde <alicia.conde85@gmail.com>
2018-06-15 11:43:06 +0200

    Empezando a trabajar con enemigos tontos

---
+++ Assets/_GameAssets/_Scripts/EnemigoTonto.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoTonto : MonoBehaviour {
+
+    [SerializeField] float velocidadMovimiento = 5;
+
+     CharacterController miCC;
+
+	private void Awake()
+	{
+        miCC = GetComponent<CharacterController>();
+	}
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        
+		
+	}
+    public void RecibirDaño(int daño)
+    {
+        
+    }
+}
