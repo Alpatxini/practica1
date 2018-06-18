e06517e820408e94742c8ca8bd1dbbf947a6968e
cice <>
2018-06-18 10:43:45 +0200

    Importado Sistema de particulas. Configurando enemigo tonto

--- Assets/_GameAssets/_Scripts/EnemigoTonto.cs
+++ Assets/_GameAssets/_Scripts/EnemigoTonto.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class EnemigoTonto : MonoBehaviour {
 
     [SerializeField] float velocidadMovimiento = 5;
+    [SerializeField] float tiempoCambioDireccion = 3;
+
+    [SerializeField] GameObject prefabParticulasMuerte;
 
      CharacterController miCC;
 
@@ -14,16 +17,28 @@ public class EnemigoTonto : MonoBehaviour {
 	}
 
 	void Start () {
+        InvokeRepeating("CambiarDireccionMovimiento", 0, tiempoCambioDireccion);
 		
 	}
 	
 	
 	void Update () {
-        
+        miCC.SimpleMove(this.transform.forward * velocidadMovimiento);
 		
 	}
+    void CambiarDireccionMovimiento()
+    {
+        float rotacionAleatoria = Random.Range(0, 360);
+        Vector3 rotacionAAplicar = new Vector3(0, rotacionAleatoria, 0);
+        this.transform.Rotate(rotacionAAplicar);
+    }
+
     public void RecibirDaño(int daño)
     {
-        
+        GameObject nuevasParticulasMuerte = Instantiate(prefabParticulasMuerte);
+        nuevasParticulasMuerte.transform.position = this.transform.position;
+
+        Destroy(this.gameObject);
     }
+	
 }
--- Assets/_GameAssets/_Scripts/Proyectil.cs
+++ Assets/_GameAssets/_Scripts/Proyectil.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Proyectil : MonoBehaviour {
 
     [SerializeField] float tiempoVida = 3;
+    [SerializeField] int daño = 1;
+
+    [SerializeField] GameObject prefabParticulasImpacto;
 
     private void Start()
 	{
@@ -13,6 +16,25 @@ public class Proyectil : MonoBehaviour {
 
 	private void OnTriggerEnter(Collider other)
 	{
+        HacerDañoAEnemigo(other);
+
+        GenerarParticulasImpacto();
+
         Destroy(this.gameObject);
 	}
+
+    private void GenerarParticulasImpacto()
+    {
+        GameObject nuevasParticulasImpacto = Instantiate(prefabParticulasImpacto);
+        nuevasParticulasImpacto.transform.position = this.transform.position;
+    }
+
+    private void HacerDañoAEnemigo(Collider other)
+    {
+        if (other.CompareTag("Enemigo"))
+        {
+            EnemigoTonto enemigo = other.GetComponent<EnemigoTonto>();
+            enemigo.RecibirDaño(daño);
+        }
+    }
 }
